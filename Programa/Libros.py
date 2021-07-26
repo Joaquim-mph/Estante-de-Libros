1983c3eb83e885951af76ef1031805ba602bb546
Ozzimandias <80657427+Joaquim-mph@users.noreply.github.com>
2021-07-26 02:21:30 -0400

    Estante v1.1
    Las funciones que modificaban el archivo ahora vuelven a cargarlo para que el estante tenga los ingresos actualizados, pequeño error en el codigo arreglado; antes al cargar el archivo no se cerraba el archivo tras finalizar. Algunos dockstrings añadidos

--- Programa/Libros.py
+++ Programa/Libros.py
@@ -1,6 +1,11 @@
 import random
 
 class Libro:
+    '''
+    Clase libro, cada uno de estos objetos representa un libro dentro de una 
+    biblioteca, los datos que contiene son nombre, autor y un booleano que indica
+    si un libro fue leido o no.
+    '''
     def __init__(self, nombre, autor,leido=False):
         assert type(nombre) == str, 'Error de formato en el nombre (str)'
         assert type(autor) == str, 'Error de formato en el autor(str)'
@@ -10,10 +15,15 @@ class Libro:
         self.leido=leido
 
     def cambiarLeido(self):
+        '''Toma un libro sin leer y lo transforma a leido'''
         self.leido = True
 
     def aString(self,preservaLeido=True, nuevoValor=True):
         '''
+        preservaLeido --> Booleano que decide si se modifica el status de leido
+        nuevoValor --> Booleano que asigna el nuevo status de leido, se usa
+                       solo cuando preservaLeido == False.
+        -----------------------------------------------------------------------
         Retorna un string con los datos del libros en el formato de una linea
         del archivo .txt necesario para que funcione la clase Estante.
         '''
@@ -39,7 +49,10 @@ libro = Libro('Bhagavad Gita', 'La vida')
 
 class Estante:
     ''' 
-    Hola
+    Esta clase esta hecha para contener una lista de libros, estos se reparten
+    entre libros leidos y libros no leidos, un trozo importante de imformacion 
+    que guarda esta clase es un archivo de texto dentro del cual se guardan los
+    libros del estante, por defecto trae un archivo con varios de ellos
     '''
     def __init__(self):
         self.libros=[]
@@ -49,6 +62,13 @@ class Estante:
         
         
     def cargarLibrosALista(self, archivo):
+        '''
+        archivo --> nombre del archivo de texto con los libros, de no haber uno
+                    lo crea por defecto como 'biblioteca.txt'
+        -----------------------------------------------------------------------
+        Este metodo se encarga de cargar los libros guardados dentro del archivo
+        .txt a objetos de la clase libro y los guarda dentro del estante
+        '''
         self.archivoEstante = archivo
         Archivo = open(archivo, "r")
         for linea in Archivo:
@@ -69,7 +89,7 @@ class Estante:
                 continue
             else:
                 continue
-        Archivo 
+        Archivo.close()
     
     def buscaNombre(self,nombre):    
         libros = self.libros
@@ -95,18 +115,23 @@ class Estante:
         assert type(nombre) == str, 'Error de formato en el nombre (str)'
         assert type(autor) == str, 'Error de formato en el autor(str)'
         assert type(leido) == bool, 'Leido debe ser un booleano'
-        
-        if leido:
-            Leido='t'
-        elif not leido:
-            Leido='f'
-        linea = [nombre+','+autor+','+Leido+'\n']
-        with open(self.archivoEstante, 'a') as f:
-                f.writelines('\n'.join(linea))
-                print(nombre+' fue agregado exitosamente!')
-    
+        busqueda = self.buscaNombre(nombre)
+        fueEncontrado = busqueda[0]       
+        if fueEncontrado:
+            print('Este libro ya se encuentra en el estante, ningun cambio realizado')
+        elif not fueEncontrado:
+            if leido:
+                Leido='t'
+            elif not leido:
+                Leido='f'
+            linea = [nombre+','+autor+','+Leido+'\n']
+            with open(self.archivoEstante, 'a') as f:
+                    f.writelines('\n'.join(linea))
+                    print(nombre+' fue agregado exitosamente!')
+                    self.cargarLibrosALista(self.archivoEstante)
     
     def eliminaLibro(self, nombre):
+        assert type(nombre) == str, 'Error de formato en el nombre (str)'
         busqueda = self.buscaNombre(nombre)
         fueEncontrado = busqueda[0]       
         numeroLinea = busqueda[1]
@@ -124,8 +149,13 @@ class Estante:
                 for n in lista:
                     textobj.write(n)
             print(libro.nombre+' fue eliminado exitosamente!')
-    
+            self.cargarLibrosALista(self.archivoEstante)
+        else:
+            print('Este libro No fue encontrado, ningún cambio realizado')
+             
+             
     def cambiaLeido(self, nombre):
+        assert type(nombre) == str, 'Error de formato en el nombre (str)'
         busqueda = self.buscaNombre(nombre)
         fueEncontrado = busqueda[0]       
         numeroLinea = busqueda[1]
@@ -141,10 +171,12 @@ class Estante:
                     else:
                         f.writelines(line)
             print(libro.nombre+' ahora figura como leido en el archivo de texto!')
+            self.cargarLibrosALista(self.archivoEstante)
         else:
             print('Este libro No fue encontrado, ningún cambio realizado')
     
     def cambiaNoLeido(self, nombre):
+        assert type(nombre) == str, 'Error de formato en el nombre (str)'
         busqueda = self.buscaNombre(nombre)
         fueEncontrado = busqueda[0]       
         numeroLinea = busqueda[1]
@@ -241,27 +273,3 @@ prueba.cargarLibrosALista('biblioteca.txt')
 prueba.verLeidos()
 print('----------------------------------------------------------------')
 prueba.verNoLeidos()
-
-#more_lines = ['Así hablo Zaratustra,F. Nietzsche,f']
-#with open('biblioteca.txt', 'a') as f:
-#    f.writelines('\n'.join(more_lines))
-
-
-#with open("biblioteca.txt",'r') as f:
-#    get_all=f.readlines()
-
-
-#with open("biblioteca.txt",'w') as f:
-#    for i,line in enumerate(get_all,1):         ## STARTS THE NUMBERING FROM 1 (by default it begins with 0)    
-#        if i == 112:                              ## OVERWRITES line:112
-#            f.writelines("Así hablo Zaratustra,Friedrich Nietzsche,f")
-#        else:
-#            f.writelines(line)
-#La mente nueva del Emperador,Roger Penrose,f
-
-
-
-
-
-
-
